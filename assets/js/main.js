dc229fb1a6fc45a4cb951a6dd4b1b59a313345ac
nomloki <luizffreitas2006@gmail.com>
2023-04-06 18:25:36 -0300

    terceiro commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,9 +14,15 @@ btn.addEventListener("click", () => {
 	// ? A tag <img>
 	let pokemonImg = document.getElementById("pokemonImg");
 	
-	// ? A <div> de abilidades
+	// ? A div habilidade
 	let pokemonHabilidade = document.getElementById("habilidade");
   
+	// ? A div formas
+	let pokemonForma = document.getElementById("formas");
+
+
+	let pokemonEspecie = document.getElementById("especies");
+
 	// ? Url da API (onde vai pegar o value do input)
 	const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
 	
@@ -30,9 +36,21 @@ btn.addEventListener("click", () => {
         pokemonImg.setAttribute(
           "src",`${dados["sprites"]["versions"]["generation-vii"]["ultra-sun-ultra-moon"]["front_default"]}`
         );
-				dados.abilities.forEach((item, index) => {
-						let abilities = document.createElement('p');
-
+				dados['abilities'].forEach((item, index) => {
+					let habilidades = document.createElement('span');
+			    habilidades.innerHTML = `${dados['abilities'][`${index}`]['ability']['name']}`+'<br>';
+          pokemonHabilidade.appendChild(habilidades);
+			});
+			dados['forms'].forEach((item, index)=>{
+				let formas = document.createElement('span');
+				formas.innerHTML = `${dados['forms'][`${index}`]['name']}`;
+				pokemonForma.appendChild(formas);
+			});
+			// ! Erro
+			dados['species'].forEach((item)=>{
+				let especies = document.createElement('span');
+				especies.innerHTML = `${dados['species']['name']}`;
+				pokemonEspecie.appendChild(especies);
 			});
       })
       .catch((_) => {
